9a55251eba8dd362e8b3dc132da763bf635c2416
IYoussef <ibraheemyoussefiy@gmail.com>
2022-11-30 21:00:05 +0000

    print digits

--- printdigits/printdigits/main.go
+++ printdigits/printdigits/main.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-	"github.com/01-edu/zo1"
-)
+import "github.com/01-edu/z01"
 
 func main() {
 	for ch := '0'; ch <= '9'; ch++ {
-		zo1.PrintRune(ch)
+		z01.PrintRune(ch)
 	}
 
 	z01.PrintRune('\n')
