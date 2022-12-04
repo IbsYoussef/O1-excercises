c567bc17a841a7877311c051db4a69cbe557d41d
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-04 12:27:07 +0000

    PrintComb

--- printcomb.go
+++ printcomb.go
@@ -6,6 +6,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := 0; i <= 9; i++ {
+		for j:= 0; j <=9; j++{
 		for k := 0; k <= 9; k++ {
 			if i < j && j < k && !(i == 7 && j == 8 && k == 9) {
 				z01.PrintRune('i')
