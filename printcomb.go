132d137ddfdef2d58c41849e13df7fa35892b93e
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-04 12:34:58 +0000

    PrintComb

--- printcomb.go
+++ printcomb.go
@@ -9,15 +9,15 @@ func PrintComb() {
 		for j := 0; j <= 9; j++ {
 			for k := 0; k <= 9; k++ {
 				if i < j && j < k && !(i == 7 && j == 8 && k == 9) {
-					z01.PrintRune('i')
-					z01.PrintRune('j')
-					z01.PrintRune('k')
+					z01.PrintRune('1')
+					z01.PrintRune('2')
+					z01.PrintRune('3')
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				} else if i == 7 && j == 8 && k == 9 {
-					z01.PrintRune('i')
-					z01.PrintRune('j')
-					z01.PrintRune('k')
+					z01.PrintRune('4')
+					z01.PrintRune('5')
+					z01.PrintRune('6')
 				}
 			}
 		}
