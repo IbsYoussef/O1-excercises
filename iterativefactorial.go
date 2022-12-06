aebfb4666d03788efcdfa508dc80081e0ab301c8
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-06 13:41:37 +0000

    Factorial

---
+++ iterativefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+// func main (){}
+
+func IterativeFactorial(nb int) int {
+	if nb >= 0 && nb < 21 {
+		var result int = 1
+		for num := 1; num <= nb; num++ {
+			result = num * result
+		}
+		return result
+	} else {
+		return 0
+	}
+}
