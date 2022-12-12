bc592ebcf5160933e096679a67e04fe1fe814028
Salaheddine HSD <jddev@noreply.learn.01founders.co>
2022-12-12 07:36:25 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -2,9 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-// func main() {}
-
-func Printcomb() {
+func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
