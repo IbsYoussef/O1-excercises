3c749086a25e589624c5d79b3c51590e70800b2e
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-15 19:29:11 +0000

    Hi

---
+++ sortwordarr.go
@@ -0,0 +1,14 @@
+package piscine
+
+// func main (){}
+
+func SortWordArr(a []string) []string {
+	for i := 0; i < len(a); i++ {
+		for j := i + 1; j < len(a); j++ {
+			if a[j] < a[i] {
+				a[j], a[i] = a[i], a[j]
+			}
+		}
+	}
+	return a
+}
