aa83505adae0bd9cbf28c13b5b7b924ad1c84536
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-15 18:43:59 +0000

    Hi

---
+++ issorted.go
@@ -0,0 +1,25 @@
+package piscine
+
+// func main(){}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	signe := 0
+	for i := 1; i < len(a); i++ {
+		if signe == 0 {
+			if f(a[i-1], a[i]) < 0 {
+				signe = -1
+			} else if f(a[i-1], a[i]) > 0 {
+				signe = 1
+			}
+		} else if signe == -1 {
+			if f(a[i-1], a[i]) > 0 {
+				return false
+			}
+		} else if signe == 1 {
+			if f(a[i-1], a[i]) < 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
