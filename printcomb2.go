f9a871e69aa31208d20a7c131ae6649ee71761c6
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-24 20:23:30 +0000

    Printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -5,11 +5,10 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			if i < j {
-				z01.PrintRune(i)
-				z01.PrintRune(j)
-				z01.PrintRune(',')
-			}
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			z01.PrintRune(',')
+
 		}
 	}
 }
