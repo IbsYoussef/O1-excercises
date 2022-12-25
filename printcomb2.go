dc84153af33f04328ace002b0ab294d6b4e93dbc
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-25 13:44:41 +0000

    PrintComb2

---
+++ printcomb2.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			z01.PrintRune(' ')
+			if j%2 == 0 {
+				z01.PrintRune(',')
+			}
+		}
+	}
+}
