9f17d71f7c8ebabf26c7b0d8ec4ecf4e5cc78775
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-25 14:28:19 +0000

    PrintComb2

--- Test/test.go
+++ Test/test.go
@@ -9,11 +9,13 @@ func main() {
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			z01.PrintRune(' ')
 			z01.PrintRune(i)
 			z01.PrintRune(j)
 			if j%2 == 1 {
 				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			} else {
+				z01.PrintRune(' ')
 			}
 		}
 	}
--- printcomb2.go
+++ printcomb2.go
@@ -5,11 +5,13 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			z01.PrintRune(' ')
 			z01.PrintRune(i)
 			z01.PrintRune(j)
 			if j%2 == 1 {
 				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			} else {
+				z01.PrintRune(' ')
 			}
 		}
 	}
