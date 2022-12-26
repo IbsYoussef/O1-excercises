b69f547a2444a8006d1cbdf5de19e07381bf4873
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-26 00:58:02 +0000

    PrintComb2

---
+++ Myprintcomb2.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func MyPrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '1'; j <= '9'; j++ {
+			z01.PrintRune('0')
+			z01.PrintRune('0')
+			z01.PrintRune(' ')
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+			if i == '9' && j == '9' {
+				break
+			}
+		}
+	}
+}
--- printcomb2.go
+++ printcomb2.go
@@ -4,17 +4,23 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '1'; j <= '9'; j++ {
-			z01.PrintRune('0')
-			z01.PrintRune('0')
-			z01.PrintRune(' ')
-			z01.PrintRune(i)
-			z01.PrintRune(j)
-			z01.PrintRune(',')
-			z01.PrintRune(' ')
-			if i == '9' && j == '9' {
-				break
+		for j := '0'; j <= '9'; j++ {
+			for k := i; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					if i*10+j < k*10+l {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(l)
+						if i != '9' || j != '8' || k != '9' || l != '9' {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
