381ef00e1af6174a2ad83ae28a1fcb1297467f3f
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-31 13:34:08 +0000

    Iterative power file

--- Test/test.go
+++ Test/test.go
@@ -1,33 +1,16 @@
-package main
+package piscine
 
-import "github.com/01-edu/z01"
+func IterativePower(nb int, power int) int {
+	multiply := nb
+	if nb == 0 {
+		return 1
+	}
+	if nb < 0 || nb > 20 {
+		return 0
+	}
+	for i := 0; i < power; i++ {
+		nb *= multiply
+	}
+	return nb
 
-func main() {
-	PrintNbr(1234)
-	PrintNbr(-9875)
-}
-
-func PrintNbr(n int) {
-	var save int
-	var tableau []int
-	var conversion int
-
-	if n < 0 {
-		z01.PrintRune('-')
-	}
-	if n == 0 {
-		z01.PrintRune('0')
-	}
-	for n != 0 {
-		save = n % 10
-		if save < 0 {
-			save = -save
-		}
-		tableau = append(tableau, save)
-		n /= 10
-	}
-	for i := len(tableau) - 1; i >= 0; i-- {
-		conversion = tableau[i] + 48
-		z01.PrintRune(rune(conversion))
-	}
 }
