f78491ab7f384fc4c7f34bb793c0c64f0bbc1f26
iyoussef <ibraheemyoussefiy@gmail.com>
2022-12-31 13:40:54 +0000

    Iterative power file

--- iterativepower.go
+++ iterativepower.go
@@ -1,1 +1,16 @@
 package piscine
+
+func IterativePower(nb int, power int) int {
+	multiply := nb
+	if nb == 0 {
+		return 1
+	}
+	if nb < 0 || nb > 20 {
+		return 0
+	}
+	for i := 0; i < power; i++ {
+		nb *= multiply
+	}
+	return nb
+
+}
