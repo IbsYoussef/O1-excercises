574423c1269c6f5727c1b0adce4b0b80239e951b
iyoussef <ibraheemyoussefiy@gmail.com>
2023-01-02 17:23:12 +0000

    NRune

--- nrune.go
+++ nrune.go
@@ -1,10 +1,10 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	if n > len(s) || n < 0 {
-		return 0
+	if n <= len(s) && n > 0 {
+		Change := []rune(s)
+		CharacterToFind := Change[n-1]
+		return CharacterToFind
 	}
-	Change := []rune(s)
-	NCharacterToFind := Change[n-1]
-	return NCharacterToFind
+	return 0
 }
