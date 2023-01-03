8f46c263e399746072855ab7b3fd6327d6ef8562
iyoussef <ibraheemyoussefiy@gmail.com>
2023-01-03 13:59:29 +0000

    IsNumeric

---
+++ isnumeric.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsNumeric(s string) bool {
+	if len(s) > 0 {
+		for i := 0; i < len(s); i++ {
+			if s[i] < 48 || s[i] > 57 {
+				return false
+			}
+		}
+	}
+	return true
+}
