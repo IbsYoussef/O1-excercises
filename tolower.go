9d6f9df09314cf90490a7d5f40375c9aabaae02a
iyoussef <ibraheemyoussefiy@gmail.com>
2023-01-03 14:04:44 +0000

    ToLower

---
+++ tolower.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ToLower(s string) string {
+	var newS string
+	if len(s) > 0 {
+		for i := 0; i < len(s); i++ {
+			if s[i] >= 65 && s[i] <= 90 {
+				newS += string(s[i] + 32)
+			} else {
+				newS += string(s[i])
+			}
+		}
+	}
+	return newS
+}
