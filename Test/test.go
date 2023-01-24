47b2e2d65a99756cba3931b302ca90c9d8fe5d01
iyoussef <ibraheemyoussefiy@gmail.com>
2023-01-24 21:44:41 +0000

    test

--- Test/test.go
+++ Test/test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	s := "Hello World!"
+	s := "ThisIsaVeryLongStringToReverse"
 	fmt.Println(StrRev(s))
 }
 
 func StrRev(s string) string {
 	var Reverse string
-	for i := len(s) - 1; i > 0; i-- {
+	for i := len(s) - 1; i >= 0; i-- {
 		Reverse += string(s[i])
 	}
 	return Reverse
